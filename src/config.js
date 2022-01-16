33661389cb31c30f211145d69ed2618c03351132
FBK45 <geliladham77@gmail.com>
2022-01-16 19:26:15 +0300

    first commit

---
+++ src/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+	discord: {
+		status: '%clientUserDisplayName%\'s account.', // The status of the Discord client.
+		statusType: 'WATCHING', // The type of the Discord status.
+		token: 'OTMyMjk5ODIxOTc5Njk3MjEz.YeQ93g.RA6O5kH8ep__QV7kq3VK50t66EI', // Discord token.
+		prefix: '@', // Discord prefix.
+		ownerIDs: ['OWNER_ID'], // The people that can use your bot.
+		ownerOnly: false // Recommended.
+	},
+
+	fortnite: {
+		cid: 'CID_028_Athena_Commando_F', // Default skin.
+		bid: 'BID_004_BlackKnight', // Default backpack.
+		eid:'EID_Floss', // Default emote.
+		pickaxe_id: 'Pickaxe_LockJaw', // Default pickaxe.
+		
+		status: 'Hey!', // Custom status.
+		platform: 'XBOX', // Platform.
+
+		acceptFriend: true, // Toggle friend requests.
+		acceptInvite: true, // Toggle party invites.
+		joinMessage: 'Hey %memberName%!', // Message to send if someone joins.
+	}
+}
